1a898b57e87ebbbb78309df885645edde8780993
isabelle <isabellezsspinheiro@gmail.com>
2024-10-13 20:19:16 -0300

    fix: Correcting header styles

--- QuebraMolas/src/components/Header/Header.jsx
+++ QuebraMolas/src/components/Header/Header.jsx
@@ -2,23 +2,38 @@ import styled from "styled-components"
 import logo from '../../assets/logo.jpg'
 
 const ContainerHeader = styled.header`
+    background-color: #fffbfb;
     display: flex;
-    justify-content: space-evenly;
+    height: 80px;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0px 80px 0px 80px;
+   
 
-    padding: 72px 94px;
 `
 
 const ContainerImg = styled.div`
+    
+
+    img{
+        display: flex;
+        border-radius: 20px;
+
+        height: 35px;
+    }
 
 `
 
 const Nav = styled.nav`
-    background-color: #FFF3CC;
-    width: 352px;
+    display: flex; 
+    align-items: center; 
+    height: 50px; 
+    /* gap: 50px; */
+    background-color: #f5e1a4;
     border-radius: 20px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    padding: 10px 20px;
+    text-align: center; 
+
 
     ul {
         display: flex;
@@ -29,25 +44,10 @@ const Nav = styled.nav`
     }
 
     li {
-        flex-grow: 1;
+        background-color: #FFC501;
+        border-radius: 30px;
+        width: 80px;
         padding: 10px 20px;
-        border-radius: 15px;
-        font-weight: bold;
-        color: #333;
-        cursor: pointer;
-    }
-
-    li:first-child {
-        background-color: #FFC501; /* Fundo amarelo para o "Home" */
-        color: #fff; /* Texto branco para contraste */
-    }
-
-    li:not(:first-child) {
-        background-color: transparent; /* Transparente para os outros itens */
-    }
-
-    li:hover {
-        background-color: #ffe58a; /* Fundo mais claro ao passar o mouse */
     }
 `
 
