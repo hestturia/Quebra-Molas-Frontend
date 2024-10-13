1677016de43470d78f33142a26203920f602c8e4
Samuel-Kepler <seu_email@example.com>
2024-10-13 19:20:34 -0300

    feat: Added authentication to the registration form

--- QuebraMolas/src/components/Formulario/Formulario.jsx
+++ QuebraMolas/src/components/Formulario/Formulario.jsx
@@ -1,29 +1,26 @@
 import styled from 'styled-components';
 import Botao from '../Botao/Botao';
 
-
 const Titulo = styled.h1`
     margin-bottom: 50px;
-`
+`;
+
 const FormularioContainer = styled.form`
     width: 65%;
-
     display: flex;
     flex-direction: column;
     align-items: center;
     margin: auto 0;
-`
+`;
 
-const Formulario = ({titulo, children, textoBotao}) => {
-    return(
-        <FormularioContainer>
+const Formulario = ({ titulo, children, textoBotao, onSubmit }) => {
+    return (
+        <FormularioContainer onSubmit={onSubmit}>
             <Titulo>{titulo}</Titulo>
-
             {children}
-
-            <Botao texto={textoBotao}/>
+            <Botao texto={textoBotao} />
         </FormularioContainer>
-    )
-}
+    );
+};
 
 export default Formulario;
