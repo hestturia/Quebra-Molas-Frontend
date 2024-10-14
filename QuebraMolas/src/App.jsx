54558032b257c183bc4b2202eec725d03013a9e6
Gabriela Reis <reisgabriela293@gmail.com>
2024-10-14 15:12:50 -0300

    Styles - add styles

--- QuebraMolas/src/App.jsx
+++ QuebraMolas/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/Login/Login';
 import Cadastro from './pages/Cadastro/Cadastro';
 import Home from './pages/Home/Home';  
 import Filter from './pages/Filter/Filter';
+import '../public/index.css';
 
 function App() {
   return (
